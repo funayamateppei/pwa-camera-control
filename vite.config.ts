bbb1c6e19156256079701260812ecf1d0e6093b8
funayamateppei <t.funayama0217@gmail.com>
2025-11-16 21:49:19 +0900

    chore: ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,8 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
+  base: process.env.NODE_ENV === 'production' ? '/pwa-camera-control/' : '/',
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
   plugins: [
     react(),
     tailwindcss(),
@@ -28,34 +25,34 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        scope: '/',
-        start_url: '/',
+        scope: process.env.NODE_ENV === 'production' ? '/pwa-camera-control/' : '/',
+        start_url: process.env.NODE_ENV === 'production' ? '/pwa-camera-control/' : '/',
         icons: [
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
       },
       injectManifest: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
       devOptions: {
         enabled: true,
-        type: 'module'
-      }
-    })
+        type: 'module',
+      },
+    }),
   ],
 })
